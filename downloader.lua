42b9a4b2738730eb8934e0cde8ed46dbe3890f6a
Epicmasonic <90917323+Epicmasonic@users.noreply.github.com>
2025-08-02 23:46:59 -0400

    Fix downloader.lua

--- downloader.lua
+++ downloader.lua
@@ -7,5 +7,5 @@ local files = {
 }
 
 for _, file in files do
-  shell.run("wget https://raw.githubusercontent.com/Epicmasonic/CraftCalc/main/craftCalc/"..craftCalc.lua.." craftCalc/"..craftCalc.lua)
+  shell.run("wget https://raw.githubusercontent.com/Epicmasonic/CraftCalc/main/craftCalc/"..file.." craftCalc/"..file)
 end
